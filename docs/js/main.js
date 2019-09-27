c17ba9e397f60bdc4681caaae859d6490cba64d0
Eugen <dev@eugenklaus.de>
2019-09-27 21:11:01 +0200

    Final Commit

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,5 +2,19 @@ $(document).ready(function() {
 
 	/* Call plugins hrere ...*/
 
+	var pull = $("#pull");
+    var nav = $("#mobile-nav");
+    
+    $(pull).on("click", function(e){
+        e.preventDefault();
+        $(nav).slideToggle();
+    });
+    
+    $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 767 && nav.is(':hidden')) {
+        nav.removeAttr('style');
+        } 
+    });
 	
 });
